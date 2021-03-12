f692e6be3db4e0714ce132110b1103f54ae52752
mateus-codes <mateus.automation@gmail.com>
2021-03-11 21:52:11 -0300

    Add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/components/**/stories.tsx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-typescript",
+    "@storybook/addon-knobs"
+  ]
+}
