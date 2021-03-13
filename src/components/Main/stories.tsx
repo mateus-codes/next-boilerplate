0406657380056667e067525785e5e062603fc444
mateus-codes <mateus.automation@gmail.com>
2021-03-13 01:29:19 -0300

    Updating storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,12 +1,13 @@
 import Main from '.'
+import { Story, Meta } from '@storybook/react/types-6-0'
 import { withKnobs, text } from '@storybook/addon-knobs'
 
 export default {
   title: 'Main',
   component: Main,
   decorators: [withKnobs],
-}
+} as Meta
 
-export const Basic = () => (
-  <Main title={text('Title', 'React Muito Avançado')} />
+export const Secondary: Story = () => (
+  <Main title={text('Title', 'React Avançado')} />
 )
