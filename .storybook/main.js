b5576fd361ff01d9de2c2d68de12dde0890ca1d6
mateus-codes <mateus.automation@gmail.com>
2021-03-13 09:48:51 -0300

    Adding controls to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,8 +4,6 @@ module.exports = {
     "../src/components/**/stories.tsx"
   ],
   "addons": [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-knobs"
   ]
 }
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,13 +1,14 @@
 import Main from '.'
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs],
 } as Meta
 
-export const Secondary: Story = () => (
-  <Main title={text('Title', 'React Avançado')} />
-)
+export const Basic: Story = (args) => <Main {...args} />
+export const Default: Story = (args) => <Main {...args} />
+Default.args = {
+  title: '123',
+  description: 'hahahah',
+}
